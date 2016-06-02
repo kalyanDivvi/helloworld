f0f5df1ea6fbb4f7f7099ff4a6ee7783f80789df
kalyan Divvi <kalyan.divvi@gmail.com>
2016-06-02 16:20:32 +0530

    added

--- sample-jenkins/src/com/java/test/MessageJUnit.java
+++ sample-jenkins/src/com/java/test/MessageJUnit.java
@@ -1,19 +1,23 @@
 package com.java.test;
 
+import junit.framework.TestCase;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MessageJUnit {
+public class MessageJUnit extends TestCase{
 	
 	MessageTest msgTest=new MessageTest();
 	
 	@Test
 	public void testAdd()
 	{
-		System.out.println("Test result :");
+		//System.out.println("Test result :");
 		int result=msgTest.add(10, 20);
 		Assert.assertEquals(result,30);
+		System.out.println("Test result :"+result);
 	}
+	
 	
 	
 }
--- sample-jenkins/src/com/java/test/MessageTest.java
+++ sample-jenkins/src/com/java/test/MessageTest.java
@@ -2,7 +2,7 @@ package com.java.test;
 
 public class MessageTest {
 	
-	public int add(int x,int y)
+	public int add(int x,int y)  throws RuntimeException
 	{
 		return x+y;
 	}
